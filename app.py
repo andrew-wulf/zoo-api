07ca0388f4ef12bc5e8b820be95658ea5b881435
Daniel Mabad <djwmasbad@gmail.com>
2024-06-26 15:00:23 -0400

    Add show action

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ def create():
     description = request.form.get("description")
     image = request.form.get("image")
     return animals_create(name, description, image)
+
+@app.route("/animals/<id>.json")
+def show(id):
+    return animals_find_by_id(id)
