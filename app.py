ed7f4a4436ab290465be1665614ddeccb5d9f0a3
Daniel Mabad <djwmasbad@gmail.com>
2024-06-26 14:57:31 -0400

    Add create action

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request
-import animals
+from animals import *
 
 app = Flask(__name__)
 
 @app.route("/animals.json")
 def index():
-    return animals.animals_all()
+    return animals_all()
 
+@app.route("/animals.json", methods=["POST"])
+def create():
+    name = request.form.get("name")
+    description = request.form.get("description")
+    image = request.form.get("image")
+    return animals_create(name, description, image)
